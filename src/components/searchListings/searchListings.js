2a66cd579086671eeacdbbfb93c0756e79711c09
RichFisher <rich.fishernc@gmail.com>
2019-01-24 19:19:39 -0600

    basic search functionality works except without plant info

--- src/components/searchListings/searchListings.js
+++ src/components/searchListings/searchListings.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import Navbar from '../navbar/navbar';
 import SearchFilter from '../searchFilter/searchFilter';
+import UserListing from '../userListing/userListing';
 import searchListingsData from '../../data/searchListingsData';
 import './searchListings.scss';
 
 class searchListings extends React.Component {
   state = {
     filtersNum: 1,
+    usersArray: '',
+    userZip: '',
+  }
+
+  componentWillMount() {
+    const user = firebase.auth().currentUser;
+    searchListingsData.getUser(user.uid)
+      .then((userData) => {
+        this.setState({ userZip: userData.location });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   newFilter = () => {
@@ -21,11 +36,40 @@ class searchListings extends React.Component {
     return renderArray;
   }
 
+  listingsBuilder = () => {
+    const listingsRender = [];
+    if (this.state.usersArray !== '') {
+      this.state.usersArray.forEach((user) => {
+        listingsRender.push(<UserListing key={user.id} location={user.location}
+        locationName={user.locationName} picture={user.picture} numRating={user.numRating}
+        qualityRating={user.qualityRating} reliabilityRating={user.reliabilityRating}
+        userName={user.userName}/>);
+      });
+    }
+    return listingsRender;
+  }
+
+  search = () => {
+    searchListingsData.getListings(this.state.userZip)
+      .then((usersArray) => {
+        this.setState({ usersArray });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="searchListings">
-      <Navbar/>
-      {this.filtersBuilder()}
+        <Navbar/>
+        <div>
+          {this.filtersBuilder()}
+          <button type='button' onClick={this.search}>Search</button>
+        </div>
+        <div>
+          {this.listingsBuilder()}
+        </div>
       </div>
     );
   }
--- src/components/userListing/userListing.js
+++ src/components/userListing/userListing.js
@@ -3,8 +3,29 @@ import './userListing.scss';
 
 class userListing extends React.Component {
   render() {
+    const {
+      picture, location, locationName, reliabilityRating, qualityRating, numRating, userName,
+    } = this.props;
     return (
       <div className="userListing">
+        <div>
+          <img src={picture} alt='profilePic'/>
+          <p>{userName}</p>
+        </div>
+        <div>
+          <div>
+            <p>Location: </p>
+            <p>{locationName} {location}</p>
+          </div>
+          <div>
+            <p>Reliability Rating: </p>
+            <p>{reliabilityRating} on {numRating} Ratings</p>
+          </div>
+          <div>
+            <p>Quality Rating: </p>
+            <p>{qualityRating} on {numRating} Ratings</p>
+          </div>
+        </div>
       </div>
     );
   }
