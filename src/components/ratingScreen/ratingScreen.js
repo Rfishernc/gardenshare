4fa24c4b2a72aab4ccecd1707ab6f9f047e9a56e
RichFisher <rich.fishernc@gmail.com>
2019-01-26 13:51:00 -0600

    reactstrap dropdown

--- src/components/ratingScreen/ratingScreen.js
+++ src/components/ratingScreen/ratingScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
+} from 'reactstrap';
 import './ratingScreen.scss';
 
 class ratingScreen extends React.Component {
+  state = {
+    dropdownOpen: false,
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
+
   render() {
     return (
       <div className='ratingScreen'>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem>Some Action</DropdownItem>
+          <DropdownItem disabled>Action (disabled)</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Foo Action</DropdownItem>
+          <DropdownItem>Bar Action</DropdownItem>
+          <DropdownItem>Quo Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
       </div>
     );
   }
