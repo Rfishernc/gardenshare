7725ba8f692d37039d370f5cae529eee809dfe60
RichFisher <rich.fishernc@gmail.com>
2019-01-29 18:36:09 -0600

    zip code filtering works, otehrs dont

--- src/data/zipcodeData.js
+++ src/data/zipcodeData.js
@@ -4,11 +4,13 @@ import apiKeys from './apiKeys';
 const zipcodeRadius = (userZip, radius) => new Promise((resolve, reject) => {
   axios.get(`https://www.zipcodeapi.com/rest/${apiKeys.zipcodeKey}/radius.json/${userZip}/${radius}/mile`)
     .then((data) => {
-      const locations = data.zip_codes;
+      const locations = data.data.zip_codes;
       const zipcodesArray = [];
-      locations.forEach((location) => {
-        zipcodesArray.push(location.zip_code);
-      });
+      if (locations !== null) {
+        locations.forEach((location) => {
+          zipcodesArray.push(location.zip_code);
+        });
+      }
       resolve(zipcodesArray);
     })
     .catch((err) => {
